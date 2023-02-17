32562b9d6854dfec0b954e69e268f994d4b31eb2
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2023-02-17 11:46:56 +0000

    listモデル

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,13 @@
 class List < ApplicationRecord
+    belongs_to :user
+    belongs_to :genre
+    has_many :favorites, dependent: :destroy
+    has_many :comments, dependent: :destroy
+  
+    validates :body,presence:true,length:{maximum:200}
+    
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
+    
 end
